310db2cfd2dcdbc66f2f51a496df24917e997281
Ngyuenhoang <104723868+Ngyuenhoang@users.noreply.github.com>
2022-05-01 22:42:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Gửi người con trai tôi iu<3",
-    introTitle: 'Hoàng',
+    titleWeb: "Gửi người con gái tôi iu<3",
+    introTitle: 'Trân',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'yêu anh nhiều lắm<3.',
+    messDesc: 'yêu em nhiều lắm<3.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
