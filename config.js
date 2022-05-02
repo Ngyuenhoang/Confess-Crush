5306ca2630545ceafff6f143301d3608cf1807e6
Ngyuenhoang <104723868+Ngyuenhoang@users.noreply.github.com>
2022-05-02 22:54:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     titleWeb: "Gửi người con gái ấy",
     introTitle: 'Quỳnh',
     introDesc: `Happy birthday`,
-    btnIntro: '^^HiHiHi^^',
+    btnIntro: '^^HiHiiHi^^',
    
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
