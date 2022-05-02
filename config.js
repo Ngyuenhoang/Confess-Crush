2035c493cbe6b83931f0d2b2929997f3ef831552
Ngyuenhoang <104723868+Ngyuenhoang@users.noreply.github.com>
2022-05-02 22:49:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,8 @@
 const CONFIG = {
-    titleWeb: "Gửi người con trai tôi thần tượng <3",
-    introTitle: 'blf_s',
-    introDesc: `Nhìn Đông sang Tây không bằng nhìn anh một giây`,
+    titleWeb: "Gửi người con gái ấy",
+    introTitle: 'Quỳnh',
+    introDesc: `Happy birthday`,
     btnIntro: '^^HiHiHi^^',
-    title: 'Phải chăng em đã yêu anh ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
-    messDesc: 'yêu em nhiều lắm<3.',
-    btnAccept: 'Okiiiii lun <3',
+   
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
