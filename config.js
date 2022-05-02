4b4df5583fec74ba4a067fdc15f12124d5e00939
Ngyuenhoang <104723868+Ngyuenhoang@users.noreply.github.com>
2022-05-02 22:59:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,5 @@ const CONFIG = {
     introTitle: 'Quỳnh',
     introDesc: `Happy birthday`,
     btnIntro: '^^HiHiiHi^^',
-   
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
